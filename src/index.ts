8fb17598c24fb06372f7b28a953f7498e64b0eef
Adriano Miranda <adrjmiranda@gmail.com>
2025-03-06 11:09:44 -0300

    feat: created script to modify the page theme color

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
 /**
  * Change Navbar Background
  */
+const ACTIVE_NAVBAR_CLASS: string = 'active';
+
 const navbar = document.getElementById('navbar') as HTMLDivElement;
 
 window.onscroll = function () {
 	if (window.scrollY > 0) {
-		navbar?.classList.add('active');
+		navbar?.classList.add(ACTIVE_NAVBAR_CLASS);
 	} else {
-		navbar?.classList.remove('active');
+		navbar?.classList.remove(ACTIVE_NAVBAR_CLASS);
 	}
 };
+
+/**
+ * Change Page Theme
+ */
+const toggleThemeButton = document.getElementById(
+	'toggle-theme'
+) as HTMLButtonElement;
+
+toggleThemeButton?.addEventListener('click', () => {
+	(toggleThemeButton.querySelectorAll('i') as NodeListOf<HTMLElement>)?.forEach(
+		(icon) => icon.classList.toggle('__hidden')
+	);
+
+	document.body.classList.toggle('__light');
+	document.body.classList.toggle('__dark');
+});
